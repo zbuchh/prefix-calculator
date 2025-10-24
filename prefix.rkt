b1f7a17d83aa6e0944616f180f44a977f02ecacf
zbuchh <zuhaib.ibuchh@gmail.com>
2025-10-24 16:43:06 -0500

    Setup mode detection and whitespace utilities

--- prefix.rkt
+++ prefix.rkt
@@ -1,1 +1,15 @@
+#lang racket
 
+(define interactive?
+  (let [(args (current-command-line-arguments))]
+    (cond
+      [(= (vector-length args) 0) #t]
+      [(string=? (vector-ref args 0) "-b") #f]
+      [(string=? (vector-ref args 0) "--batch") #f]
+      [else #t])))
+
+(define (skip-whitespace chars)
+  (cond
+    [(null? chars) '()]
+    [(char-whitespace? (car chars)) (skip-whitespace (cdr chars))]
+    [else chars]))
